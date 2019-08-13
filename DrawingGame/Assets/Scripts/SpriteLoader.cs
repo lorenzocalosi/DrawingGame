767be467480a8fd8b0df25b7bc14bf392529f8f5
Calosi <lorenzo.calosi1@gmail.com>
2019-08-13 15:57:41 +0200

    Added ScriptableObjectString, added SpriteLoader

---
+++ DrawingGame/Assets/Scripts/SpriteLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class SpriteLoader : MonoBehaviour {
+	public ScriptableObjectString path;
+
+	private void Start() {
+		byte[] png = File.ReadAllBytes(string.Concat(Application.dataPath , Path.DirectorySeparatorChar , "GeneratedSprites" , Path.DirectorySeparatorChar , "Trimmed" , Path.DirectorySeparatorChar , path.s, ".png"));
+		Texture2D newSprite = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+		newSprite.LoadImage(png);
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+		spriteRenderer.sprite = Sprite.Create(newSprite, new Rect(0, 0, newSprite.width, newSprite.height), Vector2.one / 2);
+	}
+}
