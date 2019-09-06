0952cc7e8a72063161ff8c20b2d07ea5cdb19e35
Torto96 <L.torto96fb@gmail.com>
2019-09-06 13:09:06 +0200

    Added Complete Scenes from 1.1 to 1.6
    Sprites, Animations, TImelines, Voices, Scenes, BuildSettings of all the scenes from 1.1 to 1.6

---
+++ DrawingGame/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    [SerializeField] private string OtherSceneName;
+    void OnEnable()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene(OtherSceneName, LoadSceneMode.Single);
+    }
+}
