048afe0cbe74e715637c09982895ec427fa50928
Torto96 <L.torto96fb@gmail.com>
2019-09-14 20:11:29 +0200

    Added: Draw1_Spaceship
    fix needed

---
+++ DrawingGame/Assets/Scripts/ImageLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class ImageLoader : MonoBehaviour {
+	public ScriptableObjectString path;
+
+	private void Start() {
+		byte[] png = File.ReadAllBytes(string.Concat(Application.dataPath , Path.DirectorySeparatorChar , "GeneratedSprites" , Path.DirectorySeparatorChar , "Trimmed" , Path.DirectorySeparatorChar , path.s, ".png"));
+		Texture2D newSprite = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+		newSprite.LoadImage(png);
+        UnityEngine.UI.Image spriteRenderer = GetComponent<UnityEngine.UI.Image>();
+	}
+}
